c0d2b42fc1327b7a9b55bcbb31c95881f70a9d18
mikhail <mikhaila@thoughworks.com>
2015-09-27 15:47:37 +0300

    Refactored the get instance id method to only give the id, not print it

--- aws/instance_management/list_instances.rb
+++ aws/instance_management/list_instances.rb
@@ -6,10 +6,11 @@ def get_instance_ids instances_descriptor
   instances_descriptor.each do |instance_descriptor|
     instance_id = instance_descriptor[:instances][0][:instance_id]
     instance_ids << instance_id
-    puts instance_id
   end
+  instance_ids
 end
 
 ec2 = Aws::EC2::Client.new()
 instances_descriptor = ec2.describe_instances[:reservations]
-get_instance_ids instances_descriptor
+instance_ids = get_instance_ids instances_descriptor
+puts instance_ids
