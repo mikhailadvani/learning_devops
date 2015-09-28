341e75a940a504152aa22d5e739963d32fe8c297
mikhail <mikhaila@thoughworks.com>
2015-09-28 23:09:24 +0300

    Reorganised the folder structure

--- aws/instance_management/operations/start_instances.rb
+++ aws/instance_management/operations/start_instances.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk'
 require 'pry'
-require '../../aws/instance_management/aws_instance'
+require '../../../aws/instance_management/models/aws_instance'
 instance_ids = ARGV
 instance_ids.each do |instance_id|
   AwsInstance.new(instance_id).start_instance
--- aws/instance_management/terminate_instance.rb
+++
@@ -1,7 +0,0 @@
-require 'aws-sdk'
-require 'pry'
-require '../../aws/instance_management/aws_instance'
-instance_ids = ARGV
-instance_ids.each do |instance_id|
-  AwsInstance.new(instance_id).terminate_instance dry_run=false
-end
