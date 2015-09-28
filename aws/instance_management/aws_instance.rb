3d9954bac3fa55a5ac11c1ef76c8873c5936c235
mikhail <mikhaila@thoughworks.com>
2015-09-28 21:36:39 +0300

    Moved the create instance into the AwsInstance class and added error handling to give meaningful messages rather than stack trace in case of DryRun for create instance and invalid id for start/stop instance

--- aws/instance_management/aws_instance.rb
+++ aws/instance_management/aws_instance.rb
@@ -1,19 +1,42 @@
 class AwsInstance
-  def initialize (id)
+  def initialize id=nil
     @ec2 = Aws::EC2::Client.new()
     @id = id
   end
 
+  def create_instance instance_config, dry_run=true
+    instance_config.merge!(min_count: 1, max_count: 1, dry_run: dry_run)
+    begin
+      response = @ec2.run_instances(instance_config)
+      @id = response[:instances][0][:instance_id]
+      puts "Instance created with id: #{@id}"
+    rescue Aws::EC2::Errors::DryRunOperation
+      puts "Dry run successful"
+    rescue Exception => e
+      puts "Execution unsuccessful: #{e.message}"
+    end
+  end
+
   def start_instance
-    resp = @ec2.start_instances({instance_ids: [@id]})
-    wait_until :instance_running
-    resp
+    begin
+      @ec2.start_instances({instance_ids: [@id]})
+      wait_until :instance_running
+    rescue Aws::EC2::Errors::InvalidInstanceIDNotFound => invalidIdException
+      puts invalidIdException.message
+    rescue Exception => e
+      puts e.message
+    end
   end
 
   def stop_instance
-    resp = @ec2.stop_instances({instance_ids: [@id]})
-    wait_until :instance_stopped
-    resp
+    begin
+      @ec2.stop_instances({instance_ids: [@id]})
+      wait_until :instance_stopped
+    rescue Aws::EC2::Errors::InvalidInstanceIDNotFound => invalidIdException
+      puts invalidIdException.message
+    rescue Exception => e
+      puts e.message
+    end
   end
 
   def wait_until state
--- aws/instance_management/create_instance.rb
+++ aws/instance_management/create_instance.rb
@@ -2,10 +2,7 @@ require 'aws-sdk'
 require 'pry'
 require 'json'
 require 'rails'
+require '../../aws/instance_management/aws_instance'
 template = JSON.parse(File.read("new_instance_template.json"),{:symbolize_names => true})
-ec2 = Aws::EC2::Client.new()
-response = ec2.run_instances(template)
-puts "#{response[:instances].size} instances created:"
-response[:instances].each do |instance|
-  puts instance[:instance_id]
-end
+instance = AwsInstance.new()
+instance.create_instance(template,dry_run=true)
